66ccc93bf59538da8c5eb8400039eb7be16853d8
WHALEEYE <11911413@mail.sustech.edu.cn>
2020-12-09 13:44:12 +0800

    a finished

--- Templates/quicksort.cpp
+++ Templates/quicksort.cpp
@@ -4,32 +4,6 @@ void quickSort(long long seq[], int index[], int l, int r);
 long long pivot;
 int idx, i, j;
 
-int main()
-{
-    int n, x1, x2, k, b;
-    scanf("%d%d%d", &n, &x1, &x2);
-    long long seq1[n], seq2[n];
-    int index[n];
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d%d", &k, &b);
-        seq1[i] = (long long)k * x1 + b;
-        seq2[i] = (long long)k * x2 + b;
-        index[i] = i;
-    }
-    quickSort(seq1, index, 0, n - 1);
-    for (int i = 0; i < n - 1; i++)
-    {
-        if (seq2[index[i]] > seq2[index[i + 1]])
-        {
-            printf("YES");
-            return 0;
-        }
-    }
-    printf("NO");
-    return 0;
-}
-
 void quickSort(long long seq[], int index[], int l, int r)
 {
     if (l < r)
