255e0d7fb104dd24f04d9e34d0c11492ba70abf2
WHALEEYE <11911413@mail.sustech.edu.cn>
2020-12-15 19:24:05 +0800

    A finished + heap Template

--- Lab09/A.cpp
+++ Lab09/A.cpp
@@ -6,6 +6,29 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-
+    int t, n, m, **gra, ver1, ver2;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> m;
+        gra = new int *[n];
+        for (int i = 0; i < n; i++)
+        {
+            gra[i] = new int[n]();
+        }
+        while (m--)
+        {
+            cin >> ver1 >> ver2;
+            gra[ver1 - 1][ver2 - 1]++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cout << gra[i][j] << " ";
+            }
+            cout << '\n';
+        }
+    }
     return 0;
 }
