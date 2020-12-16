7e7bc81cc576db5fcbb4a42a9daa5d7db15ae891
WHALEEYE <11911413@mail.sustech.edu.cn>
2020-12-16 19:11:41 +0800

    Update B.cpp

--- Lab09/B.cpp
+++ Lab09/B.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
 using namespace std;
 
+struct Node
+{
+    int value;
+    Node *next;
+    Node *pre;
+};
+
+struct GraphNode
+{
+    int ind;
+    bool visited;
+    Node *head;
+};
+
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-
+    int n, m, u, v;
+    bool hasCir;
+    cin >> n >> m;
+    int *stk = new int[m];
+    int *grp = new int[m + 1];
+    while (m--)
+    {
+        
+    }
     return 0;
 }
