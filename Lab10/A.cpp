1993e470690c6d0883e3f8c8574c996d544c6f70
WHALEEYE <11911413@mail.sustech.edu.cn>
2021-01-02 23:27:56 +0800

    Lab10 A+B finished

--- Lab10/A.cpp
+++ Lab10/A.cpp
@@ -1,10 +1,69 @@
+#include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
+
+struct Edge {
+  int end;
+  int weight;
+  bool visited;
+};
+
+struct GraphNode {
+  int idx;
+  bool visited;
+  vector<Edge *> nbs;
+  GraphNode(int idx) : idx(idx), visited(false) {}
+};
+
+bool comp(Edge *a, Edge *b) { return a->weight < b->weight; }
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   cout.tie(0);
-  
+  int n, m, min, u, v, w, minend = 1;
+  long long sum = 0;
+  cin >> n >> m;
+  GraphNode **grp = new GraphNode *[n + 1];
+  Edge *chosen;
+  for (int i = 1; i <= n; i++) {
+    grp[i] = new GraphNode(i);
+  }
+  for (int i = 0; i < m; i++) {
+    cin >> u >> v >> w;
+    grp[u]->nbs.push_back(new Edge{v, w});
+    grp[v]->nbs.push_back(new Edge{u, w});
+  }
+  for (int i = 1; i <= n; i++) {
+    sort(grp[i]->nbs.begin(), grp[i]->nbs.end(), comp);
+  }
+  bool stop = false;
+  grp[1]->visited = true;
+  while (!stop) {
+    min = 10000000;
+    stop = true;
+    for (int i = 1; i <= n; i++) {
+      if (grp[i]->visited) {
+        for (int j = 0; j < grp[i]->nbs.size(); j++) {
+          if ((!grp[grp[i]->nbs[j]->end]->visited) &&
+              (!grp[i]->nbs[j]->visited)) {
+            if (grp[i]->nbs[j]->weight < min) {
+              stop = false;
+              chosen = grp[i]->nbs[j];
+              min = grp[i]->nbs[j]->weight;
+            }
+            break;
+          }
+        }
+      }
+    }
+    if (!stop) {
+      sum += min;
+      grp[chosen->end]->visited = true;
+      chosen->visited = true;
+    }
+  }
+  cout << sum;
   return 0;
 }
--- Lab10/B.cpp
+++ Lab10/B.cpp
@@ -1,10 +1,105 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+struct Edge {
+  int st;
+  int ed;
+};
+
+struct GraphNode {
+  int idx;
+  int curidx;
+  bool visited;
+  vector<Edge *> nbs;
+};
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   cout.tie(0);
-  
+  int n, m, u, v;
+  cin >> n >> m;
+  GraphNode **grp = new GraphNode *[n + 1];
+  GraphNode **grpT = new GraphNode *[n + 1];
+  for (int i = 1; i <= n; i++) {
+    grp[i] = new GraphNode{i, 0, false};
+    grpT[i] = new GraphNode{i, 0, false};
+  }
+  for (int i = 0; i < m; i++) {
+    cin >> u >> v;
+    grp[u]->nbs.push_back(new Edge{u, v});
+    GraphNode *temp1 = grp[u];
+    grpT[v]->nbs.push_back(new Edge{v, u});
+    GraphNode *temp2 = grp[v];
+  }
+
+  GraphNode **stk = new GraphNode *[n];
+  GraphNode *topE;
+  bool back = false;
+  int top = 0, num = 0;
+
+  stk[top++] = grpT[1];
+  grpT[1]->visited = true;
+  while (top != 0) {
+    topE = stk[top - 1];
+    int size = topE->nbs.size();
+    for (int i = topE->curidx; i < topE->nbs.size(); i++) {
+      if (!grpT[topE->nbs[i]->ed]->visited) {
+        topE->curidx = i;
+        break;
+      }
+      if (i == topE->nbs.size() - 1) {
+        topE->curidx = topE->nbs.size();
+      }
+    }
+    if (topE->curidx < topE->nbs.size()) {
+      stk[top] = grpT[topE->nbs[topE->curidx]->ed];
+      grpT[topE->nbs[topE->curidx]->ed]->visited = true;
+      top++;
+      back = false;
+    } else {
+      num++;
+      top--;
+      back = true;
+    }
+  }
+  if (num != n) {
+    cout << "wawawa";
+    return 0;
+  }
+
+  back = false;
+  num = 0;
+  stk[top++] = grp[1];
+  grp[1]->visited = true;
+  while (top != 0) {
+    topE = stk[top - 1];
+    for (int i = topE->curidx; i < topE->nbs.size(); i++) {
+      if (!grp[topE->nbs[i]->ed]->visited) {
+        topE->curidx = i;
+        break;
+      }
+      if (i == topE->nbs.size() - 1) {
+        topE->curidx = topE->nbs.size();
+      }
+    }
+    if (topE->curidx < topE->nbs.size()) {
+      stk[top] = grp[topE->nbs[topE->curidx]->ed];
+      grp[topE->nbs[topE->curidx]->ed]->visited = true;
+      top++;
+      back = false;
+    } else {
+      num++;
+      top--;
+      back = true;
+    }
+  }
+  if (num != n) {
+    cout << "wawawa";
+    return 0;
+  }
+
+  cout << "Bravo";
   return 0;
 }
